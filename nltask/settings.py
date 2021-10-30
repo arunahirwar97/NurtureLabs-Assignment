b7e0b1d577e1cf59723cb7bec3c2e7f8bbd044aa
websiteaap <arun887126@gmail.com>
2021-10-30 22:49:32 +0530

    Add fianl project

--- nltask/settings.py
+++ nltask/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-02j6%z4)*h5906x%%w2b1s!ew+x(v0%@8q-st_h+$*+y(rn1q4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
